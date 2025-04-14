07bf59422446e585df1c57d423ab6ee7225a61ab
Alex Odysseus David <alexxodavid@gmail.com>
2025-04-14 02:05:10 +0000

    Part 3: Looping Through Arrays

--- arrayWorksheet.js
+++ arrayWorksheet.js
@@ -2,7 +2,7 @@ let fruits = ["apple", "banana", "cherry"];
 console.log(fruits[1]);
 console.log(fruits.length);
 
-
+// Part 2: Adding and Removing Elements
 
 fruits.push("orange");
 console.log(fruits); 
@@ -22,3 +22,26 @@ let anotherRemovedFruit = fruits.pop();
 
 console.log(fruits); 
 console.log("Length of fruits array:", fruits.length);
+
+
+// Part 3: Looping Through Arrays
+
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i].toUpperCase());
+}
+
+
+let appleCount = 0;
+
+    for (let i = 0; i < fruits.length; i++) {
+        if (fruits[i] === "apple") {
+            appleCount++;
+        }
+    }   
+console.log("Number of apples in the array:", appleCount);
