5302fa387081571f53bebe15772ac0fce4325312
Alex Odysseus David <alexxodavid@gmail.com>
2025-04-14 02:02:20 +0000

    Part 2: Adding and Removing Elements

--- arrayWorksheet.js
+++ arrayWorksheet.js
@@ -4,3 +4,21 @@ console.log(fruits.length);
 
 
 
+fruits.push("orange");
+console.log(fruits); 
+
+
+let removedFruit = fruits.pop();
+console.log(fruits);
+console.log(removedFruit); 
+
+
+fruits.push("pineapple");
+fruits.push("strawberry");
+
+
+let anotherRemovedFruit = fruits.pop();
+
+
+console.log(fruits); 
+console.log("Length of fruits array:", fruits.length);
